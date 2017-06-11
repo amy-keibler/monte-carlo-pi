bca5c0725a6d304a9f69cc4c1bdd0c92dd71940d
Jane Keibler <wanderingelf14@gmail.com>
2017-06-11 19:19:21 -0400

    Cleaned up Code
    Pulled out common functionality in preparation to move it into the
    library instead of in the main app

--- monte-carlo-pi-hs/app/Main.hs
+++ monte-carlo-pi-hs/app/Main.hs
@@ -18,12 +18,16 @@ numSamples = 1000000
 imageRadius :: Num a => a
 imageRadius = 1024
 
+imageDiameter :: Num a => a
+imageDiameter = 2 * imageRadius
+
 samples :: StdGen -> [MCI.Point]
 samples = take numSamples . randoms
 
 transformPoint :: MCI.Point -> [Primitive]
-transformPoint (MCI.Point x y) = circle (V2 (double2Float ((x + 1) * imageRadius))
-                                          (double2Float ((y + 1) * imageRadius))) 1
+transformPoint (MCI.Point x y) = circle (V2 (double2Float $ toImageScale x)
+                                          (double2Float $ toImageScale y)) 1
+  where toImageScale num = (num + 1) * imageRadius
 
 inPoints :: [MCI.Point] -> [[Primitive]]
 inPoints points = fmap transformPoint inPts
@@ -38,20 +42,21 @@ main = do
   gen <- getStdGen
   fontResult <- loadFontFile "/usr/share/fonts/noto/NotoMono-Regular.ttf"
   case fontResult of
-        Left err -> print $ "ERROR: " ++ err
+        Left err -> print err
         Right font -> writePng "/tmp/pi.png" $ createPNG gen font
 
 createPNG :: StdGen -> Font -> Image PixelRGBA8
-createPNG gen font = renderDrawing (2 * imageRadius) (2 * imageRadius) white $ do
-  withTexture (uniformTexture blue) $ mapM_ fill $ inPoints points
-  withTexture (uniformTexture red) $ mapM_ fill $ outPoints points
-  withTexture (uniformTexture black) $ mapM_ (stroke 4 JoinRound (CapRound, CapRound)) [circle (V2 imageRadius imageRadius) imageRadius,
-                                                                                         line (V2 0 imageRadius) (V2 (2 * imageRadius) imageRadius),
-                                                                                         line (V2 imageRadius 0) (V2 imageRadius (2 * imageRadius))]
-  withTexture (uniformTexture black) $ printTextAt font (PointSize 72) (V2 (imageRadius + 64) (imageRadius - 64)) (show pi)
+createPNG gen font = renderDrawing imageDiameter imageDiameter white $ do
+  withColor blue $ mapM_ fill $ inPoints points
+  withColor red $ mapM_ fill $ outPoints points
+  withColor black $ mapM_ (stroke 4 JoinRound (CapRound, CapRound)) [circle (V2 imageRadius imageRadius) imageRadius,
+                                                                     line (V2 0 imageRadius) (V2 imageDiameter imageRadius),
+                                                                     line (V2 imageRadius 0) (V2 imageRadius imageDiameter)]
+  withColor black $ printTextAt font (PointSize 72) (V2 (imageRadius + 64) (imageRadius - 64)) $ show pi
   where white = PixelRGBA8 255 255 255 255
         black = PixelRGBA8 0 0 0 255
         blue = PixelRGBA8 255 0 0 255
         red = PixelRGBA8 0 0 255 255
         points = samples gen
         pi = approximatePi points
+        withColor = withTexture . uniformTexture
